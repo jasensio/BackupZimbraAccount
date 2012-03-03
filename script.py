d53822cae52f8dac6fa3ad6bdfbe0fc1a07cf7c1
Your Name <jero.asensio@gmail.com>
2012-03-04 00:34:19 +0100

    Cambio menor.

--- script.py
+++ script.py
@@ -32,7 +32,7 @@ def user_import(mailbox):
     cmd = 'zmaccts | grep '+ mailbox + ' | cut -d " " -f1'
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     p.wait()
-    out, error = p.communicate()[0]
+    out = p.communicate()[0]
     out = out[:-1]
     if out != mailbox:
         print "No existe el buzón, creándolo..."
@@ -48,7 +48,7 @@ def user_import(mailbox):
     cmd = 'ls -lh backup_'+mailbox+'_.tgz | cut -d " " -f 5'
     p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     p.wait()
-    out, error = p.communicate()[0]
+    out = p.communicate()[0]
     out = out[:-1]
     print "Importando la cuenta "+mailbox+" con un tamaño de: "+out
     cmd = 'zmmailbox -z -m '+mailbox+' postRestURL "//?fmt=tgz&resolve=reset"  backup_'+mailbox+'_.tgz'
@@ -98,7 +98,7 @@ def user_export_massive():
                 cmd = 'zmmailbox -z -m ' + mail + ' gms'
                 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
                 p.wait()
-                out, error = p.communicate()[0]
+                out = p.communicate()[0]
                 out = out[:-1]
                 print "Exportando la cuenta: " + mail + "de tamaño: " + out
                 cmd = 'zmmailbox -z -m ' + mail + ' getRestURL "//?fmt=tgz" > backup_' + mail + '_.tgz'
@@ -116,7 +116,7 @@ def user_export_massive():
                 cmd = 'zmmailbox -z -m ' + mail + ' gms'
                 p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
                 p.wait()
-                out, error = p.communicate()[0]
+                out = p.communicate()[0]
                 out = out[:-1]
                 print "Exportando la cuenta: " + mail + "de tamaño: " + out
                 cmd = 'zmmailbox -z -m ' + mail + ' getRestURL "//?fmt=tgz" > backup_' + mail + '_.tgz'
@@ -144,7 +144,7 @@ def user_export(mailbox):
          cmd = 'zmmailbox -z -m ' + mailbox + ' gms'
          p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
          p.wait()
-         out, error = p.communicate()[0]
+         out = p.communicate()[0]
          out = out[:-1]
          print "Exportando el buzón " + mailbox + " con un tamaño de " + out
          cmd = 'zmmailbox -z -m ' + mailbox + ' getRestURL "//?fmt=tgz" > backup_' + mailbox + '_.tgz'
