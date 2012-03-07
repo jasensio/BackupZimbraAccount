d512f28f518ceb3ea1cdefc8624ba9b59590bb4d
jasensio <jasensio@integrafmagenta.com>
2012-03-07 13:56:20 +0100

    Depuracion.

--- script.py
+++ script.py
@@ -49,6 +49,7 @@ def user_import(mailbox):
     else:
         f = open(mailbox + '.txt')
         params = f.read()
+        print "Parametros: " + params
         print "El buzón a importar existe, se sobreescribirán los datos..."
         question = raw_input("CONTINUAR??  (SI) ")
         if question != "SI":
